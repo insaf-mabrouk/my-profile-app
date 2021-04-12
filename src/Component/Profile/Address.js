d1919fc479f887cc9c4f31ac52f54c101421cb52
unknown <insafmabrouk@gmail.com>
2021-04-12 13:12:41 +0100

    profie app

---
+++ src/Component/Profile/Address.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Address = () => {
+    return (
+        <div>
+            <h3>Address: Cité Erriadh Rue Farhat Hached Sened Gafsa</h3>
+            
+        </div>
+    )
+}
+
+export default Address
---
+++ src/Component/Profile/ProfilPhoto.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProfilPhoto = () => {
+    return (
+        <div>
+            <img src ={"/me.jpg"} style={{height:250,width:250, borderRadius:150}} />
+        </div>
+    )
+}
+
+export default ProfilPhoto
